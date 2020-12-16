ec88afd4a7adff3b2970afc4006cd437d820cba2
Ignat Zakrevsky <iezakrevsky@gmail.com>
2020-12-16 19:06:19 +0300

    Fix sequence formatter for equal values

--- lib/ordered_sequence_formatter.rb
+++ lib/ordered_sequence_formatter.rb
@@ -7,7 +7,7 @@ class OrderedSequenceFormatter
     formatter ||= ->(x) { x.to_s }
 
     [nil, *@list, nil].each_cons(3).map { |before, current, after|
-      next([formatter.call(current), formatter.call(after)].join("=")) if after && current == after
+      next([formatter.call(current), formatter.call(after)].sort.join("=")) if after && current == after
       next(nil) if before && current == before
       formatter.call(current)
     }.compact
